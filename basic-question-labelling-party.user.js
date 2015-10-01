4dd9f1a06f2878ac70f993ea7e6c236142a7f658
Dean Ward <dean.ward@bakedbean.org.uk>
2015-10-01 17:15:29 -0400

    Create basic-question-labelling-party.user.js

---
+++ basic-question-labelling-party.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Basic Question Labelling Party
+// @namespace    http://stackoverflow.com/
+// @version      0.1
+// @description  Hides everything but the 
+// @author       Dean Ward
+// @match        http://stackoverflow.com/questions/*
+// @grant        none
+// ==/UserScript==
+
+$(".vote").hide();
+$("#answers").hide();
+$("#sidebar").hide();
+$(".comment").hide();
